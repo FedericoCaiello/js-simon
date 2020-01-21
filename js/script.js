95758f771abf8b3fb29ab2f88416490d772a8b66
Federico <federico.caiello@gmail.com>
2020-01-21 18:36:55 +0100

    creazione punteggio con esito

--- js/script.js
+++ js/script.js
@@ -1,24 +1,48 @@
-// Da li parte un timer di 30 secondi.
-// Dopo 30 secondi l'utente deve inserire un prompt alla volta i numeri che ha visto precedentemente.
-// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati
 
 // Un alert espone 5 numeri casuali.
-var numeriCasuali = [];
-
+var numeriCasuali = [10,];
+var arrayPC = [];
+var punteggio = 0;
 for (var i = 1; i < 6; i++) {
   numeriCasuali.push(getRandomIntInclusive(1, 100));
 }
-console.log(numeriCasuali);
 alert('5 numeri casuali da 1 a 100 ' + numeriCasuali);
 
+// Da li parte un timer di 30 secondi.
+// Dopo 30 secondi l'utente deve inserire un prompt alla volta i numeri che ha visto precedentemente.
+var i=0;
+setTimeout(function () {
+  while(i<5) {
+    var numeroUtente = parseInt(prompt('inserisci un numero'));
+    arrayPC.push(numeroUtente);
+    console.log(arrayPC);
+    i++;
+  }
+  var z=0;
+  while (z<5){
+    if (numeriListaInArray(numeriCasuali, arrayPC[z]) == true){
+      punteggio++;
+    }
+    z++;
+  }
+  console.log(punteggio);
+
+}, 1000);
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati
 
 
+// FUNZIONI
 
-
-
-
-
-
+function numeriListaInArray(lista, numero) {
+  for (var i = 0; i < lista.length; i++) {
+    if (lista[i] == numero) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+}
 
 
 
